3e29c2abeb92baefd5284ef46c54c3ef0863b170
Sameer Alam <104647142+sameer1098alam@users.noreply.github.com>
2024-04-08 17:11:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './Appa.css';
+import Navbar from './Navbar';
+import TextForm from './TextForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">   
+        <Navbar title="my-app"/>
+        <div className="container">
+          <TextForm heading="Enter the text to analyze"/>
+        </div>
+      </div>    
+    </>
   );
 }
 
